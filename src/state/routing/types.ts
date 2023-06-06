e200732a0722b8ab113be3cdb069c5ddc0378ebc
sandro <devspuig@pm.me>
2023-06-06 16:00:56 +0200

    Remove unused `@uniswap/router-sdk` dependency

--- src/state/routing/types.ts
+++ src/state/routing/types.ts
@@ -1,6 +1,5 @@
 import { BigNumber } from '@ethersproject/bignumber'
-import { Trade } from '@uniswap/router-sdk'
-import { Currency, CurrencyAmount, Price, Token, TradeType } from '@uniswap/sdk-core'
+import { Currency, CurrencyAmount, Price, TradeType } from '@uniswap/sdk-core'
 import type { ChainId } from '@uniswap/smart-order-router'
 import { RouterPreference } from 'hooks/routing/types'
 import { QuoteRequest, QuoteResult, Step } from 'wido'
@@ -13,42 +12,6 @@ export enum TradeState {
 }
 
 // from https://github.com/Uniswap/routing-api/blob/main/lib/handlers/schema.ts
-
-export type TokenInRoute = Pick<Token, 'address' | 'chainId' | 'symbol' | 'decimals'>
-
-export type V3PoolInRoute = {
-  type: 'v3-pool'
-  tokenIn: TokenInRoute
-  tokenOut: TokenInRoute
-  sqrtRatioX96: string
-  liquidity: string
-  tickCurrent: string
-  fee: string
-  amountIn?: string
-  amountOut?: string
-
-  // not used in the interface
-  address?: string
-}
-
-export type V2Reserve = {
-  token: TokenInRoute
-  quotient: string
-}
-
-export type V2PoolInRoute = {
-  type: 'v2-pool'
-  tokenIn: TokenInRoute
-  tokenOut: TokenInRoute
-  reserve0: V2Reserve
-  reserve1: V2Reserve
-  amountIn?: string
-  amountOut?: string
-
-  // not used in the interface
-  // avoid returning it from the client-side smart-order-router
-  address?: string
-}
 
 export interface GetQuoteArgs {
   tokenInAddress: string
@@ -69,11 +32,6 @@ export const INITIALIZED = 'Initialized'
 export const NO_ROUTE = 'No Route'
 
 export type GetQuoteResult = QuoteResult | typeof INITIALIZED | typeof NO_ROUTE
-
-/**
- * @deprecated
- */
-export class InterfaceTrade extends Trade<Currency, Currency, TradeType> {}
 
 export interface WidoTradeType<TradeInput extends Currency, TradeOutput extends Currency> {
   inputAmount: CurrencyAmount<TradeInput>
