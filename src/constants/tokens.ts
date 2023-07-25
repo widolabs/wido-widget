3a90906c555ba30e39cd6982b7337b458da8422f
sandro <devspuig@pm.me>
2023-07-25 12:34:38 +0700

    add BNB native

--- src/constants/tokens.ts
+++ src/constants/tokens.ts
@@ -447,6 +447,13 @@ export const WRAPPED_NATIVE_CURRENCY: { [chainId: number]: Token | undefined } =
     'WMATIC',
     'Wrapped MATIC'
   ),
+  [SupportedChainId.BSC]: new Token(
+    SupportedChainId.BSC,
+    '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c',
+    18,
+    'WBNB',
+    'Wrapped BNB'
+  ),
 }
 
 export function isCelo(chainId: number): chainId is SupportedChainId.CELO | SupportedChainId.CELO_ALFAJORES {
