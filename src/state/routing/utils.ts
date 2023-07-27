9a32f635f88bfbc14a3de650a4d4025c708aa2dc
sraver <devspuig@pm.me>
2023-07-27 14:35:53 +0200

    Remove unused `@uniswap/router-sdk` dependency (#10)
    * Remove unused `@uniswap/router-sdk` dependency
    
    * Remove other packages that had @uniswap/router-sdk as dependency
    
    ---------
    
    Co-authored-by: Kunal Jain <jainkunal3004@gmail.com>
    Co-authored-by: Kunal <kunal@joinwido.com>

--- src/state/routing/utils.ts
+++ src/state/routing/utils.ts
@@ -1,10 +1,11 @@
-import { ONE } from '@uniswap/router-sdk'
 import { Currency, CurrencyAmount, Fraction, Percent } from '@uniswap/sdk-core'
+import JSBI from 'jsbi'
 
 export function calcMinimumAmountOut(
   slippageTolerance: Percent,
   amountOut: CurrencyAmount<Currency>
 ): CurrencyAmount<Currency> {
+  const ONE = JSBI.BigInt(1)
   const slippageAdjustedAmountOut = new Fraction(ONE).subtract(slippageTolerance).multiply(amountOut.quotient).quotient
   return CurrencyAmount.fromRawAmount(amountOut.currency, slippageAdjustedAmountOut)
 }
