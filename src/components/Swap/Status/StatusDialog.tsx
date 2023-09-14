eb75890255a629d4e1721535b1a127c36b93448c
Roman <30427286+barabanovro@users.noreply.github.com>
2023-09-14 11:50:44 +0400

    Add transaction substatus (#21)
    * Add substatus
    
    * Add failed check
    
    * Add icons and substatus text
    
    * Upgrade wido sdk
    
    ---------
    
    Co-authored-by: Hesham <hesham@joinwido.com>

--- src/components/Swap/Status/StatusDialog.tsx
+++ src/components/Swap/Status/StatusDialog.tsx
@@ -1,8 +1,9 @@
 import { Trans } from '@lingui/macro'
 import ErrorDialog, { StatusHeader } from 'components/Error/ErrorDialog'
 import SwapSummary from 'components/Swap/Summary'
+import { getChainInfo } from 'constants/chainInfo'
 import { MS_IN_SECOND } from 'constants/misc'
-import { LargeArrow, LargeCheck, LargeSpinner } from 'icons'
+import { LargeAlert, LargeArrow, LargeCheck, LargeSpinner, LargeWarning } from 'icons'
 import { useEffect, useMemo, useState } from 'react'
 import { SwapTransactionInfo, Transaction, TransactionType } from 'state/transactions'
 import { ThemedText, TransitionDuration } from 'theme'
@@ -13,13 +14,20 @@ import Column from '../../Column'
 interface TransactionStatusProps {
   tx: Transaction
   dstTxHash?: string
+  dstTxSubStatus?: string
   onClose: () => void
 }
 
-function TransactionStatus({ tx, onClose, dstTxHash }: TransactionStatusProps) {
+function TransactionStatus({ tx, onClose, dstTxHash, dstTxSubStatus }: TransactionStatusProps) {
   const fromChainId = (tx.info as SwapTransactionInfo).trade.fromToken.chainId
   const toChainId = (tx.info as SwapTransactionInfo).trade.toToken.chainId
   const isSingleChain = fromChainId === toChainId
+  const toChainInfo = getChainInfo(toChainId)
+
+  const isPartiallySuccessful = useMemo(
+    () => !!tx.receipt?.status && !!dstTxHash && dstTxSubStatus !== 'completed',
+    [dstTxHash, dstTxSubStatus, tx.receipt?.status]
+  )
 
   const [showConfirmation, setShowConfirmation] = useState(true)
   const Icon = useMemo(() => {
@@ -28,12 +36,18 @@ function TransactionStatus({ tx, onClose, dstTxHash }: TransactionStatusProps) {
     }
 
     if (tx.receipt?.status) {
+      if (dstTxHash && dstTxSubStatus === 'failed') {
+        return LargeAlert
+      }
+      if (dstTxHash && dstTxSubStatus !== 'completed') {
+        return LargeWarning
+      }
       if (dstTxHash || isSingleChain) {
         return LargeCheck
       }
     }
     return LargeSpinner
-  }, [showConfirmation, tx.receipt?.status, dstTxHash, isSingleChain])
+  }, [showConfirmation, tx.receipt?.status, dstTxHash, isSingleChain, dstTxSubStatus])
 
   useEffect(() => {
     // We should show the confirmation for 1 second,
@@ -51,6 +65,12 @@ function TransactionStatus({ tx, onClose, dstTxHash }: TransactionStatusProps) {
       return <Trans>Transaction submitted</Trans>
     } else if (tx.info.type === TransactionType.SWAP) {
       if (tx.receipt?.status) {
+        if (dstTxHash && dstTxSubStatus === 'failed') {
+          return <Trans>Unsuccessful</Trans>
+        }
+        if (dstTxHash && dstTxSubStatus !== 'completed') {
+          return <Trans>Partial Success</Trans>
+        }
         if (dstTxHash || isSingleChain) {
           return <Trans>Success</Trans>
         }
@@ -62,7 +82,7 @@ function TransactionStatus({ tx, onClose, dstTxHash }: TransactionStatusProps) {
       return tx.receipt?.status ? <Trans>Success</Trans> : <Trans>Unwrap pending</Trans>
     }
     return tx.receipt?.status ? <Trans>Success</Trans> : <Trans>Transaction pending</Trans>
-  }, [showConfirmation, tx.info.type, tx.receipt?.status, dstTxHash, isSingleChain])
+  }, [showConfirmation, tx.info.type, tx.receipt?.status, dstTxHash, isSingleChain, dstTxSubStatus])
 
   const subheading = useMemo(() => {
     if (isSingleChain && tx.receipt?.status) {
@@ -71,6 +91,23 @@ function TransactionStatus({ tx, onClose, dstTxHash }: TransactionStatusProps) {
     if (tx.receipt?.status && !dstTxHash) {
       return <Trans>Waiting for the second transaction...</Trans>
     }
+    if (tx.receipt?.status && dstTxHash && dstTxSubStatus === 'failed') {
+      return <Trans>Your transaction was Unsuccessful</Trans>
+    }
+    if (isPartiallySuccessful) {
+      return tx.info.type === TransactionType.SWAP ? (
+        <Trans>
+          Your funds were successfully bridged,
+          <br />
+          but the transaction on {toChainInfo?.label} failed.
+          <br />
+          <br />
+          To finalise your Zap, swap your tokens to {tx.info.trade.toToken.symbol} on {toChainInfo?.label}.
+        </Trans>
+      ) : (
+        <Trans>Your transaction was partially successful</Trans>
+      )
+    }
     if (tx.receipt?.status && dstTxHash) {
       return <Trans>Your transaction was successful</Trans>
     }
@@ -78,11 +115,14 @@ function TransactionStatus({ tx, onClose, dstTxHash }: TransactionStatusProps) {
       return <Trans>Waiting for the transaction...</Trans>
     }
     return <Trans>Waiting for the first transaction...</Trans>
-  }, [tx.receipt?.status, dstTxHash, isSingleChain])
+  }, [tx.receipt?.status, dstTxHash, isSingleChain, dstTxSubStatus, isPartiallySuccessful])
 
   return (
-    <Column flex padded gap={0.75} align="stretch" style={{ height: '100%' }}>
-      <StatusHeader icon={Icon} iconColor={tx.receipt?.status ? 'success' : undefined}>
+    <Column flex padded gap={0.75} align="stretch" style={{ height: '100%', maxWidth: '100%' }}>
+      <StatusHeader
+        icon={Icon}
+        iconColor={tx.receipt?.status ? (dstTxSubStatus !== 'completed' ? 'warning' : 'success') : undefined}
+      >
         <ThemedText.H4>{heading}</ThemedText.H4>
         {tx.info.type === TransactionType.SWAP ? (
           <SwapSummary
@@ -91,9 +131,10 @@ function TransactionStatus({ tx, onClose, dstTxHash }: TransactionStatusProps) {
             srcTxHash={tx.info.response.hash}
             dstTxHash={dstTxHash}
             isSingleChain={isSingleChain}
+            isPartiallySuccessful={isPartiallySuccessful}
           />
         ) : null}
-        <ThemedText.Subhead1>{subheading}</ThemedText.Subhead1>
+        <ThemedText.Subhead1 sx={{ maxWidth: '100%', whiteSpace: 'normal' }}>{subheading}</ThemedText.Subhead1>
       </StatusHeader>
       <ActionButton onClick={onClose}>
         <Trans>Close</Trans>
@@ -102,7 +143,7 @@ function TransactionStatus({ tx, onClose, dstTxHash }: TransactionStatusProps) {
   )
 }
 
-export default function TransactionStatusDialog({ tx, onClose, dstTxHash }: TransactionStatusProps) {
+export default function TransactionStatusDialog({ tx, onClose, dstTxHash, dstTxSubStatus }: TransactionStatusProps) {
   return tx.receipt?.status === 0 ? (
     <ErrorDialog
       header={<Trans>Your swap failed.</Trans>}
@@ -117,6 +158,6 @@ export default function TransactionStatusDialog({ tx, onClose, dstTxHash }: Tran
       onClick={onClose}
     />
   ) : (
-    <TransactionStatus tx={tx} dstTxHash={dstTxHash} onClose={onClose} />
+    <TransactionStatus tx={tx} dstTxHash={dstTxHash} dstTxSubStatus={dstTxSubStatus} onClose={onClose} />
   )
 }
--- src/components/Swap/index.tsx
+++ src/components/Swap/index.tsx
@@ -48,10 +48,14 @@ function usePoll(timeout: number) {
       }, delay)
     }
     loop(1, timeout)
-    return () => clearTimeout(t)
-  }, [])
+    return () => {
+      clearTimeout(t)
+    }
+  }, [timeout])
 
-  return timer
+  const resetTimer = () => setTimer(0)
+
+  return { timer, resetTimer }
 }
 
 export default function Swap(props: SwapProps) {
@@ -59,7 +63,7 @@ export default function Swap(props: SwapProps) {
   useSyncController(props as SwapController)
   useSyncConvenienceFee(props as FeeOptions)
   useSyncSwapEventHandlers(props as SwapEventHandlers)
-  const timer = usePoll(30000)
+  const { timer, resetTimer } = usePoll(15000)
 
   const [wrapper, setWrapper] = useState<HTMLDivElement | null>(null)
 
@@ -70,6 +74,7 @@ export default function Swap(props: SwapProps) {
     [txHash, pendingTxs]
   )
   const [dstTxHash, setDstTxHash] = useState<string | undefined>()
+  const [dstTxSubStatus, setDstTxSubStatus] = useState<string | undefined>()
   const [txCompleted, setTxCompleted] = useState<boolean>(false)
 
   useEffect(() => {
@@ -79,10 +84,10 @@ export default function Swap(props: SwapProps) {
     getStatus({
       chainId: tx.info.trade.fromToken.chainId,
       txHash: tx.receipt.transactionHash,
-    }).then(({ status, toTxHash }) => {
-      if (status === 'success') {
-        setTxCompleted(true)
-      }
+    }).then(({ status, toTxHash, substatus, substatusMessage }) => {
+      if (status === 'success') setTxCompleted(true)
+      if (status === 'success' && substatus === undefined) setDstTxSubStatus('completed')
+      else setDstTxSubStatus(substatus)
       setDstTxHash(toTxHash)
     })
   }, [tx, txCompleted, timer])
@@ -115,11 +120,15 @@ export default function Swap(props: SwapProps) {
           <StatusDialog
             tx={tx}
             dstTxHash={dstTxHash}
+            dstTxSubStatus={dstTxSubStatus}
             onClose={() => {
               setDisplayTxHash()
               setDstTxHash(undefined)
+              setDstTxSubStatus(undefined)
               setSnFetchedBalances({})
               setEvmFetchedBalances({})
+              resetTimer()
+              setTxCompleted(false)
             }}
           />
         </Dialog>
